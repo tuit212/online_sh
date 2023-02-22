62cebc57a02b995f675ff0ff41d081b1e692fb8c
tuit212 <asadbekanvarov9808@example.com>
2023-02-22 07:44:06 +0500

    ..

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from "./components/Header/Header";
 import Discounts from "./common/Discounts/Discounts.jsx";
 import Main from "./components/Main/Main";
 import NotFound from "./common/404/NotFound";
+import Like from "./common/Like/Like";
+import Card from "./common/Card/Card";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Main/>} />
         <Route path="/Discounts" element={<Discounts/>} />
+        <Route path="/like" element={<Like/>} />
+        <Route path="/cart" element={<Card/>} />
         <Route path="*" element={<NotFound/>} />
       </Routes>
     </div>
--- src/components/Header/LikeCard.jsx
+++ src/components/Header/LikeCard.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from 'react' 
+import { Link } from 'react-router-dom'
 
 function LikeCard() {
   return (
     <>
       <ul className='header__card'>
-        <li className='header__card-item' title='Sevimlilar'>
+        <Link to={"/like"} className='header__card-item' title='Sevimlilar'>
             <div><i class="fa fa-heart"></i></div>
             <p>Sevimlilar</p>
             <span id='Sevimlilar'>0</span>
-        </li>
-        <li className='header__card-item' title='Korzinka' >
+        </Link>
+        <Link to={"/cart"} className='header__card-item' title='Korzinka' >
             <div><i class="fa-solid fa-cart-shopping" id='Korzinka'></i></div>
             <p>Korzinka</p>
             <span id='Korzinka'>0</span>
-        </li>
+        </Link>
         <li className='header__card-item' title='Kirish'>
             <div><i class="fa-sharp fa-solid fa-truck"></i></div>
             <p>Truck</p>
